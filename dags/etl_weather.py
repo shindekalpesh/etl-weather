8e106bb5238f3f606fabffa0851f9f581c0a53b4
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2025-10-11 09:33:44 +0530

    fixed dependency import issue.

--- dags/etl_weather.py
+++ dags/etl_weather.py
@@ -1,6 +1,22 @@
 from airflow import DAG
-from airflow.providers.https.hooks.http import HttpHook                         # Fetch data from API
+from airflow.providers.http.hooks.http import HttpHook                         # Fetch data from API
+
 from airflow.providers.postgres.hooks.postgres import PostgresHook              # Push data into Postgres
 from airflow.decorators import task
 from airflow.utils.dates import days_ago
 
+
+# Latitude and Longitude for the desired location. (Using Switzerland in this case.)
+
+LATITUDE = '46.8182'
+LONGITUDE = '8.2275'
+POSTGRES_CONN_ID = 'postgres_default'
+API_CONN_ID = 'open_meteo_api'
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    
+}
+
+print("default_args", type(default_args), default_args)
